6692ad09f1556fb387253a3d944862b756af9155
Sam Yapkowitz <samyapkowitz@gmail.com>
2023-06-07 18:42:05 -0500

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -131,7 +131,7 @@ const Home: NextPage = () => {
             rows={4}
             className="w-full rounded-md border-gray-300 shadow-sm focus:border-black focus:ring-black my-5"
             placeholder={
-              "e.g. intech, ecommerce, insurance, health, real estate, transportation, finserv"
+              "e.g. fintech, ecommerce, insurance, health, real estate, transportation, finserv"
             }
           />
           {/* <div className="flex mb-5 items-center space-x-3">
